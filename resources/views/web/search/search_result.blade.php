2d1f2b5f38a690ca56da9e9d48619f59f2c3a710
suraj-repositories <suraj2002fake@gmail.com>
2025-03-15 14:39:25 +0530

    fixed search page massive size problem

--- resources/views/web/search/search_result.blade.php
+++ resources/views/web/search/search_result.blade.php
@@ -7,39 +7,48 @@
         </div>
     </div>
 @else
-
+    @php $counter = 0; @endphp
     @foreach ($resultBranches as $branch)
-    <div class="{{ $loop->iteration > 1 ? 'item-border' : '' }}">
-        <a class="item page" href="{{ route('branches.show', $branch->id) }}">
-            <div class="icon-box">
-                <iconify-icon icon="lets-icons:file-duotone" class="icon"></iconify-icon>
-            </div>
-            <div class="text-box">
-                <div class="title">{{ $branch->name }}</div>
-                <iconify-icon icon="lsicon:right-outline" class="right-arrow"></iconify-icon>
-            </div>
-        </a>
-
-        @foreach ($branch->books as $book)
-        <a class="item content" href="{{ route('books.show', $book->id) }}">
-            <div class="icon-box">
-                <iconify-icon icon="solar:notebook-broken" class="icon transparent"></iconify-icon>
-            </div>
-            <div class="text-box sub-border">
-                <div class="title">
-                    <div>{{ $book->title }}
-                        @if($book->author && str_contains(strtolower($book->author), strtolower($search)))
-                            <br> <small><b>Author : </b>{{ $book->author }}</small>
-                        @endif
-                    </div>
-                    <small data-html-to-text='true'> {!! $book->description !!} </small>
+        @if ($counter >= 30)
+            @break;
+        @endif
+        @php $counter++; @endphp
+        <div class="{{ $loop->iteration > 1 ? 'item-border' : '' }}">
+            <a class="item page" href="{{ route('branches.show', $branch->id) }}">
+                <div class="icon-box">
+                    <iconify-icon icon="lets-icons:file-duotone" class="icon"></iconify-icon>
                 </div>
-                <iconify-icon icon="si:down-right-line" class="right-arrow"></iconify-icon>
-            </div>
-        </a>
-        @endforeach
+                <div class="text-box">
+                    <div class="title">{{ $branch->name }}</div>
+                    <iconify-icon icon="lsicon:right-outline" class="right-arrow"></iconify-icon>
+                </div>
+            </a>
 
-    </div>
+
+            @foreach ($branch->books as $book)
+                @if ($counter >= 30)
+                    @break;
+                @endif
+                @php $counter++; @endphp
+                <a class="item content" href="{{ route('books.show', $book->id) }}">
+                    <div class="icon-box">
+                        <iconify-icon icon="solar:notebook-broken" class="icon transparent"></iconify-icon>
+                    </div>
+                    <div class="text-box sub-border">
+                        <div class="title">
+                            <div>{{ $book->title }}
+                                @if ($book->author && str_contains(strtolower($book->author), strtolower($search)))
+                                    <br> <small><b>Author : </b>{{ $book->author }}</small>
+                                @endif
+                            </div>
+                            <small data-html-to-text='true'> {!! $book->description !!} </small>
+                        </div>
+                        <iconify-icon icon="si:down-right-line" class="right-arrow"></iconify-icon>
+                    </div>
+                </a>
+            @endforeach
+
+        </div>
     @endforeach
 
 @endif
